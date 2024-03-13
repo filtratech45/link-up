08ad95001aa459a0cf0db304a32156ce28113d19
Damien Levacher <damien@dlcode.fr>
2024-03-13 11:02:47 +0100

    build only in PRODUCTION

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    // output: 'export',
-    // distDir: 'build',
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js';
+ 
+const nextConfig = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+        ...defaultConfig,
+        images: {
+          unoptimized: true,
+        },
+    }
+  }
+ 
+  /** @type {import('next').NextConfig} */
+  return {
+    ...defaultConfig,
+    output: 'export',
+    distDir: 'build',
     images: {
       unoptimized: true,
     },
-};
+  }
+}
 
 export default nextConfig;
